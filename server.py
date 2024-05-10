b3b60853462061a0f920aba8c34dc6c9062f1b6d
morning120429 <forever120429@gmail.com>
2024-05-10 16:18:13 +1000

    first commit

---
+++ server.py
@@ -0,0 +1,47 @@
+import requests
+from time import sleep
+
+telegram_token = '6623261193:AAHDyIMqw8xtWo1y_zxa4Br_yjuWJXlzJwo'
+telegram_chat_id = '6360011882'
+
+# Server's address
+server_address = 'https://app.slack.com/client/T032XS1SL0M/D06S9D3B3PU'
+
+def send_telegram_message(message):
+
+    sendData = {
+        "chat_id" : telegram_chat_id,
+        "text" : message,
+    }
+
+    telegramURL = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
+    response = requests.post(telegramURL, sendData)
+
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print("Error sending message: {}".format(response.status_code))
+
+def check_server():
+    try:
+        response = requests.get(server_address, timeout = 30)
+        if response.status_code == 200:
+            return 'Server is started'
+        else:
+            return 'Server is not responding normally'
+    except requests.exceptions.RequestException as e:
+        return 'Server is stopped'
+
+def main():
+    last_status = None
+    while True:
+        current_status = check_server()
+        if current_status != last_status:
+            send_telegram_message(message = current_status)
+            last_status = current_status
+        else:
+            print('None change server')
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
---
+++ server1.py
@@ -0,0 +1,50 @@
+import requests
+import threading
+from time import sleep
+
+telegram_token = '6623261193:AAHDyIMqw8xtWo1y_zxa4Br_yjuWJXlzJwo'
+telegram_chat_id = '6360011882'
+
+# Server's address
+server_address = 'https://app.slack.com/client/T032XS1SL0M/D06S9D3B3PU'
+
+last_status = None
+
+def send_telegram_message(message):
+
+    sendData = {
+        "chat_id" : telegram_chat_id,
+        "text" : message,
+    }
+
+    telegramURL = f'https://api.telegram.org/bot{telegram_token}/sendMessage'
+    response = requests.post(telegramURL, sendData)
+
+    if response.status_code == 200:
+        print("Message sent successfully!")
+    else:
+        print("Error sending message: {}".format(response.status_code))
+
+def check_server():
+    global last_status
+    try:
+        response = requests.get(server_address, timeout = 30)
+        print('Send get request')
+        status = response.status_code
+    except requests.exceptions.RequestException as e:
+        status = str(e)
+
+    if status != last_status:
+        if isinstance(status, int) and status == 200:
+            send_telegram_message(f'Server is up: {status}')
+        else:
+            send_telegram_message(f'Server issue detected: {status}')
+        last_status = status
+
+def main():
+    while True:
+        threading.Thread(target=check_server).start()
+        sleep(1)
+
+if __name__ == '__main__':
+    main()
